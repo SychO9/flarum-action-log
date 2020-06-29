574da6a69234142b574f10593db99ce8e310bbe1
SychO9 <sychocouldy@gmail.com>
2020-06-29 15:40:04 +0100

    Create table migration

---
+++ migrations/2020_06_25_000000_create_logs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @package Flarum Logs
+ * @author Sami "SychO" Mazouz (https://github.com/SychO9)
+ * @version 0.1.0
+ * @license MIT
+ */
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\Builder;
+
+return [
+    'up' => function (Builder $schema) {
+        if ($schema->hasTable('action_log_entries')) {
+            return;
+        }
+
+        $schema->create('action_log_entries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('actor_id')->unsigned();
+            $table->string('name');
+            $table->string('type');
+            $table->integer('resource_id')->unsigned()->nullable();
+            $table->string('resource_type')->nullable();
+            $table->json('data')->nullable();
+            $table->dateTime('created_at');
+        });
+    },
+
+    'down' => function (Builder $schema) {
+        $schema->dropIfExists('action_log_entries');
+    },
+];
